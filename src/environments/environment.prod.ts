b822ce0459b76f124cd4f586417f64e32740fceb
Gillian Barreto Arias <gillianbarretoa@gmail.com>
2022-10-13 08:12:27 -0300

    config: añadir url api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { general } from './general';
 
 const specific = {
   production: true,
-  API_URL: 'http://localhost:3000',  // TODO: Cambiar
+  API_URL: 'https://api-transferencias.herokuapp.com',
   API_BASE: '/api/v1'
 };
 
